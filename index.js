25035d5d4f1f1121ea7ef8e0c8d51c85c7972598
Lisset-Mejia <clissetmg@gmail.com>
2023-10-21 13:03:59 -0600

    Avance

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+//Dependencies
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
+//Rputers
+const employee = require('./routes/employee');
 
-app.get("/", (req, rest, next) => {
+// falta hacerlo 
+const user = require('./routes/user')
 
-});
+//Middleware
+const auth = require('./middleware/auth');
+const notFound = require ('./middleware/notFound');
+const index = require ('./middleware/index');
+const cors = require ('./middleware/cors');
 
-app.listen(3000, () => {
-    console.log("yeah");
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", index);
+app.use("/user", user);
+app.use(auth);
+app.use("/employee", employee);
+app.use(notFound);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running...");
 });
